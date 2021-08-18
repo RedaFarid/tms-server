eeac26e396724f447ce8f520d244a1f447abdab6
Reda farid <reda.farid.abdelhamid@gmail.com>
2021-08-18 14:41:52 +0200

    Initial commit

---
+++ src/test/java/TMSserver/TmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package TMSserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
